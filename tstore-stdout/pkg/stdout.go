926b755106976744dd5f90b46e3051a9bc31887f
markmnl <markmnl@outlook.com>
2020-02-02 20:28:53 +0800

    ParentExists() func

--- tstore-stdout/pkg/stdout.go
+++ tstore-stdout/pkg/stdout.go
@@ -6,11 +6,16 @@ import (
 	"github.com/markmnl/tmail-store/tstore/pkg"
 )
 
+// ParentExists always returns true since this store doesn't know
+func ParentExists(msg *tstore.Msg) (bool, error) {
+	return true, nil
+}
+
 // Store pretty prints msg supplied to stdout
 func Store(msg *tstore.Msg) error {
 	_, err := fmt.Printf(`
 id:     %s
-pid     %s
+pid:    %s
 from:   %s
 to:     %s
 time:   %s
